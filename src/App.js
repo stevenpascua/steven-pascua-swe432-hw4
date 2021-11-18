6fb842310c5471113e01cfd3ffb6da90c39e1d31
slp20028 <steven.pascua98@gmail.com>
2021-11-18 15:22:14 -0500

    Init

---
+++ src/App.js
@@ -0,0 +1,168 @@
+import React, { Component } from 'react';
+import './App.css';
+
+function AppHeader()  
+{
+
+    return (
+      <div className="Header">
+          <div>
+            <h1>Super Smash Bros Skill Simulator</h1>
+            <p>This webapp takes a persons kill count in super smash bros and decides who is the better player using complex math and statistics! Enter the player's kill count below to get started! </p>
+          </div>
+          </div>
+      );    
+}
+
+function ListPlayers(props)
+{
+	const players = props.players;
+	const listItems = players.map((players) =>
+		<li key={players.toString()}>
+		{players}
+		</li>
+	);
+	return (
+		<ul>{listItems}</ul>
+	);
+}
+	
+function Result(){
+	const resultMessage = ["Anthony sucks yo lmao", "Idk who got the most skillz but Anthoy def has the least lmao.","Anthony Sucks at smash bros", "Anthony is not good with Rosalina and Luma", "Anthony got no skills bro", "Anthony is trash at Smash dawg","Somebody tell Anthony to stop playing villager because he's not good with him."]
+	var randomNum = Math.floor(Math.random() * resultMessage.length);
+	return (
+		<b>{resultMessage[randomNum]}</b>
+	)
+
+}
+
+function Calculation(){
+  const [numberSequence, numberHolder] = React.useState('');
+
+  let numbers = 'Calculating............... Done',
+  index = React.useRef(0);
+
+  React.useEffect(() => {
+    function iterate() {
+      numberHolder(prev => prev + numbers[index.current]);
+      index.current++;
+    }
+    if (index.current < numbers.length) {
+      let addChar = setInterval(iterate, 50);
+      return () => clearInterval(addChar);
+    }
+  }, [numberSequence]);
+
+  return (
+    <div>
+		{numberSequence}<br>
+		</br>
+		{index.current == numbers.length-1?
+			<Result />
+			:null
+		}
+    </div>
+  )
+}
+/** This component displays the characters and gives the user input for their kill count.
+
+
+
+ */
+class KillCount extends Component {
+	
+	constructor(props)
+	{
+		super(props);
+		this.state = {
+		    stevenCount: '',
+		    gabeCount: '',
+		    liganCount: '',
+			anthonyCount: '',
+			players: [],
+			countSubmit: false,		
+		};
+		
+		this.labelChangedS = this.labelChangedS.bind(this);
+		this.labelChangedL = this.labelChangedL.bind(this);
+		this.labelChangedG = this.labelChangedG.bind(this);
+		this.labelChangedA = this.labelChangedA.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	
+	}
+	labelChangedS(event)
+	{
+		this.setState({stevenCount:event.target.value})	
+	}
+	labelChangedL(event)
+	{
+		this.setState({liganCount:event.target.value})	
+	}
+	labelChangedG(event)
+	{
+		this.setState({gabeCount:event.target.value})	
+	}
+	labelChangedA(event)
+	{
+		this.setState({anthonyCount:event.target.value})	
+	}
+	handleSubmit(event) 
+	{
+		if ((isNaN(this.state.stevenCount) || this.state.stevenCount == '') || (isNaN(this.state.gabeCount) || this.state.gabeCount == '') || (isNaN(this.state.liganCount) || this.state.liganCount == '') || (isNaN(this.state.anthonyCount) || this.state.anthonyCount == ''))
+		{
+			alert("Error: all players need at least one number and just one number.")
+		}
+	 	else
+		{
+			this.setState({countSubmit:true,players:["Steven: " + String(this.state.stevenCount),"Gabe: " + String(this.state.gabeCount),"Ligan: " + String(this.state.liganCount),"Anthony: " + String(this.state.anthonyCount)]});
+		}
+		event.preventDefault();
+ 	} 
+	
+	render(){
+		const messages = ["fetching from sources...", "loading account..."];
+			return (
+			<div className="App">
+				<div>
+					<h3> Highest Count: </h3>
+					<h3> Lowest Count: </h3>
+					{!this.state.countSubmit ?
+					<div>
+						<h2> Enter each players kill count: </h2>
+							<label>
+							Steven: <input type = "text" value = {this.state.stevenCount} onChange={this.labelChangedS}/> <br>
+							</br>
+							Ligan: <input type = "text" value = {this.state.liganCount} onChange={this.labelChangedL}/> <br>
+							</br>
+							Gabe: <input type = "text" value = {this.state.gabeCount} onChange={this.labelChangedG}/> <br>
+							</br>
+							Anthony: <input type = "text" value = {this.state.anthonyCount} onChange={this.labelChangedA}/>
+							</label>
+						<form onSubmit={this.handleSubmit}>
+							<input type="submit" value="Submit" />
+						</form>
+							<div className="Pepe">
+							<p>SPIN</p>
+								<img src={require('./images/pepe.jpg').default}/>
+
+							</div>
+					</div>
+					:
+					<div>
+						<ListPlayers players={this.state.players} />
+						<div className="mathContainer">
+						<Calculation />
+						</div>
+					</div> 
+				}
+			
+			
+			 	</div>
+		 	</div>
+	
+			)	
+		}
+}
+
+
+export {AppHeader, KillCount};
